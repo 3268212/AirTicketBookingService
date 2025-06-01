458515f814d14f3dc1e85337b5308c5b593ddc2a
Sameer Ansari <sam3268212@gmail.com>
2025-06-01 21:57:09 +0530

    Added more function to RabbitMQ

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -15,8 +15,16 @@ class BookingController {
 
   async sendMessageToQueue(req, res) {
         const channel = await createChannel();
-        const data = {message: 'Success' };
-        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        const payload = {
+           data: {
+            subject: 'This is a noti from queue',
+            content: 'Some queue will subscribe this',
+            recepientEmail: 'cs19297@gmail.com',
+            notificationTime: '2025-05-31T18:59:00'
+           },
+           service: 'CREATE_TICKET'
+        };
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
         return res.status(200).json({
           message: 'Successfully published the event'
         });
--- src/index.js
+++ src/index.js
@@ -9,8 +9,11 @@ const setupAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    // app.get('/bookingservice/api/v1/home', (req, res) => {
+    //     return res.json({message: 'Hitting the booking service'});
+    // })
     
-    app.use('/api', apiRoutes);
+    app.use('/bookingservice/api', apiRoutes);
     
 
     app.listen(PORT, () => {
